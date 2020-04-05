3fee234c5c166be091091a7a70432c9cc8afcab1
fordring <131071634@qq.com>
2020-04-05 14:07:24 +0800

    update

---
+++ url-scheme-demo/src/main/java/icu/fordring/UrlSchemeDemoApplication.java
@@ -0,0 +1,13 @@
+package icu.fordring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlSchemeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlSchemeDemoApplication.class, args);
+    }
+
+}
